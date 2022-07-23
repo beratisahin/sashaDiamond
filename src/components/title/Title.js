a1611a1c1a87f95cee3848365cefa6f795c3f94e
beratisahin <beratisahin94@gmail.com>
2022-07-23 03:25:34 +0300

    third commit

--- src/components/title/Title.js
+++ src/components/title/Title.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import "./Title.css";
-import YL from "./Circle.png"
+import YL from "./Circle.png";
+import SDL from "./sdl.png";
+import S1 from "../header/SashaDiamondMenuLogo.png";
 
 export default class Title extends Component {
     render() {
@@ -8,8 +10,8 @@ export default class Title extends Component {
         const {page_title} = this.props;
         return (
             <div className="page_header" style={{borderRadius:"5px",height:"9rem"}}>
-                    <img src={YL} className="page_header_icon" style={{width:"9rem",height:"9rem",float:"left"}}></img>
-                    <a className="page_header_home" href="/">Ana Sayfa <i class="fas fa-long-arrow-alt-right" style={{color:"#E1DFDF"}}></i></a>
+                    <img src={S1} className="page_header_icon" style={{width:"12rem",height:"9rem",float:"left"}}></img>
+                    <a className="page_header_home" href="/">Ana Sayfa <i class="fas fa-long-arrow-alt-right"></i></a>
                     
                     <p className="page_header_title">{page_title}</p>
               
